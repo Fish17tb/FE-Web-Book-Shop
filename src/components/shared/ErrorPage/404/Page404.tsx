20d8e2071e9c733f5db939d8037b5b6bb3e10e7b
Hoàng Nguyên Vũ <fish17tb@gmail.com>
2025-08-24 20:58:59 +0700

    Handle-Protected-Page

---
+++ src/components/shared/ErrorPage/404/Page404.tsx
@@ -0,0 +1,29 @@
+import "../error.scss";
+
+const Page404 = () => {
+  return (
+    <div className="container" >
+      {/* Clouds */}
+      <div id="clouds">
+        <div className="cloud x1"></div>
+        <div className="cloud x1_5"></div>
+        <div className="cloud x2"></div>
+        <div className="cloud x3"></div>
+        <div className="cloud x4"></div>
+        <div className="cloud x5"></div>
+      </div>
+
+      {/* Content */}
+      <div className="c">
+        <div className="_404">404</div>
+        <div className="_1">THE PAGE</div>
+        <div className="_2">WAS NOT FOUND</div>
+        <a className="btn" href="/">
+          BACH HOME
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Page404;
--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,10 @@ import LoginPage from "pages/client/auth/login.tsx";
 import RegisterPage from "pages/client/auth/register";
 import HomePage from "pages/client/home.tsx";
 import { AppProvider } from "./utils/context/app.context";
+import ProtectedRound from "./components/shared/ProtectedRound/ProtectedRound";
+import LayoutAdmin from "./pages/admin/layoutAdmin";
+import Page403 from "./components/shared/ErrorPage/403/Page403";
+import Page404 from "./components/shared/ErrorPage/404/Page404";
 
 const router = createBrowserRouter([
   {
@@ -21,6 +25,38 @@ const router = createBrowserRouter([
         path: "/contacts",
         element: <div>Xin chao VN</div>,
       },
+      {
+        path: "/checkout",
+        element: (
+          <ProtectedRound>
+            <div>this is page checkout</div>
+          </ProtectedRound>
+        ),
+      },
+      {
+        path: "/order",
+        element: (
+          <ProtectedRound>
+            <div>this is page order</div>
+          </ProtectedRound>
+        ),
+      },
+      {
+        path: "/history",
+        element: (
+          <ProtectedRound>
+            <div>this is page history</div>
+          </ProtectedRound>
+        ),
+      },
+      {
+        path: "/admin",
+        element: (
+          <ProtectedRound>
+            <LayoutAdmin />
+          </ProtectedRound>
+        ),
+      },
     ],
   },
   {
@@ -30,6 +66,16 @@ const router = createBrowserRouter([
   {
     path: "/register",
     element: <RegisterPage />,
+  },
+
+  //  Route riêng cho lỗi
+  {
+    path: "/403",
+    element: <Page403 />,
+  },
+  {
+    path: "*", // bất kỳ path nào không khớp sẽ vào đây
+    element: <Page404 />,
   },
 ]);
 
